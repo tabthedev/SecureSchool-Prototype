f6a13fd7b60dca583aae2265fae9237a2c54f819
탭・tab・タブ <taehui@anchored.kr>
2025-09-25 15:04:10 +0900

    Update WidgetService.cpp
    시간 연산 수정

--- WidgetService/WidgetService.cpp
+++ WidgetService/WidgetService.cpp
@@ -1,4 +1,4 @@
-﻿#include <cstdlib>
+#include <cstdlib>
 #include <time.h>
 #include <Windows.h>
 #include <thread>
@@ -48,31 +48,32 @@ int main() {
 	int i = 0;
 
 	while (true) {
-		Sleep(1000);
+		Sleep(100);
 
 		time_t seconds = time(NULL);
 		int seconds_int = (int)seconds + 32400;
 		int secFromDay = seconds_int % 86400;
+		float minFromDay = secFromDay / 60;
 		int secFromWeek = (seconds_int + 345600) % 604800;
 
 		(518400 >= secFromWeek && secFromWeek >= 0) ? printf("t1 ") : printf("f1 ");
 		(34980 >= secFromDay && secFromDay >= 34500) ? printf("t2\n") : printf("f2\n");
-		printf("%i  ..  %i\n", secFromDay, secFromWeek);
+		std::cout << secFromDay << ", " << int(minFromDay * 100) << " .. " << seconds_int << std::endl;
 
 		if (
 			(518400 >= secFromWeek && secFromWeek >= 0) // 월~금
-
-			&&
-
-			((34980 >= secFromDay && secFromDay >= 34985) || //1교시 후 5초
-				(38280 >= secFromDay && secFromDay >= 38285) || //2교시 후 5초
-				(41580 >= secFromDay && secFromDay >= 41585) || //3교시 후 5초
-				(47280 >= secFromDay && secFromDay >= 47285) || //4교시 후 5초
-				(50580 >= secFromDay && secFromDay >= 50585)) //5교시 후 5초
 			)
 		{
 			i++;
-			system(i % 2 == 0 ? "taskkill /f /im chrome.exe && PowerShell -Command \"Start-Process -WindowStyle Hidden PowerShell \\\"Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]::Show('프로그램이 자동 종료 되었습니다', '週中 쉬는 시간 감지됨', 'Ok','Asterisk')\\\"\"" : "taskkill /f /im msedge.exe && PowerShell -Command \"Start-Process -WindowStyle Hidden PowerShell \\\"Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]::Show('프로그램이 자동 종료 되었습니다', '週中 쉬는 시간 감지됨', 'Ok','Asterisk')\\\"\"");
+			if (
+				(578 >= minFromDay && minFromDay >= 570) ||
+				(578 + 45 * 1 >= minFromDay && minFromDay >= 570 + 45 * 1) ||
+				(578 + 45 * 2 >= minFromDay && minFromDay >= 570 + 45 * 2) ||
+				(838 >= minFromDay && minFromDay >= 570 + 45 * 3) ||
+				(850 + 45 * 1 >= minFromDay && minFromDay >= 840 + 45 * 1)
+				) {
+				system(i % 2 == 0 ? "taskkill /f /im chrome.exe && PowerShell -Command \"Start-Process -WindowStyle Hidden PowerShell \\\"Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]::Show('프로그램이 자동 종료 되었습니다', '주중 쉬는 시간 감지됨', 'Ok','Asterisk')\\\"\"" : "taskkill /f /im msedge.exe && PowerShell -Command \"Start-Process -WindowStyle Hidden PowerShell \\\"Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]::Show('프로그램이 자동 종료 되었습니다', '주중 쉬는 시간 감지됨', 'Ok','Asterisk')\\\"\"");
+			}
 
 		}
 	}
